b0d0d777b4b5448ee15f9482e39d101ad81d1acf
CipiVlad <supercoder.cipi@gmail.com>
2022-05-11 17:09:38 +0200

    weather

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // console.log("text");
 
-// input-number
-inputAge = document.getElementById("input-number");
+// input-Age
+let inputAge = document.getElementById("input-number");
 // Deklariere die Funktion adult()
 
 function adult() {
@@ -16,4 +16,32 @@ function adult() {
         document.getElementById('status').innerHTML = 'minderjährig';
     }
 };
+
+
+// input-Weather-Quality
+let inputQ = document.getElementById('input-number');
+
+function weather() {
+    inputQ = document.getElementById("input-qualitynumber").value;
+    // console.log(inputQ);
+
+    if (inputQ >= 8 && inputQ <= 10) {
+        console.log('super');
+        document.getElementById('qstatus').innerHTML = 'super';
+    } else if (inputQ >= 6 && inputQ <= 7) {
+        console.log('gut');
+        document.getElementById('qstatus').innerHTML = 'gut';
+    }
+    else if (inputQ >= 3 && inputQ <= 5) {
+        console.log('okay');
+        document.getElementById('qstatus').innerHTML = "okay";
+    } else if (inputQ >= 0 && inputQ <= 2) {
+        console.log('miserabel');
+        document.getElementById('qstatus').innerHTML = "miserabel";
+    } else {
+        console.log('invalid number');
+        document.getElementById('qstatus').innerHTML = "invalid number, try again!"
+    }
+};
+
 
